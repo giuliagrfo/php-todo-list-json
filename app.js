c27f2ce20f2b5f2aea5133d4eb8ba48704fba5ee
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-30 16:44:52 +0100

    add file js

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            tasks: [],
+            apiUrl: 'server.php'
         }
     },
     methods: {
-
+        getTasks(url) {
+            axios.get(url)
+                .then(resp => {
+                    console.log(resp);
+                    this.tasks = resp.data
+                })
+                .catch(err => {
+                    console.error(err.message);
+                })
+        }
     },
     mounted() {
-
+        this.getTasks(this.apiUrl);
     }
 }).mount('#app')
